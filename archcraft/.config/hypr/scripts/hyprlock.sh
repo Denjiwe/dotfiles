fed62a1be9ba51d34b27f604d0a3aae08d660920
Santhiago Monteiro <santhiago.monteiro@hotmail.com>
2024-04-12 15:07:17 -0300

    Update dotfiles

---
+++ archcraft/.config/hypr/scripts/hyprlock.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+playerctl pause -q
+
+$HOME/.config/hypr/scripts/wallchange.sh
+
+wallpaper_path_one=$(wpaperctl get-wallpaper eDP-1 | tr -d '"')
+if [[ "$wallpaper_path_one" != *.png ]]; then
+    wallpaper_path_one=/usr/share/backgrounds/9.png
+else
+    wallpaper_path_one=$HOME/.current_wallpaper_eDP-1
+fi
+
+wallpaper_path_two=$(wpaperctl get-wallpaper HDMI-A-1 | tr -d '"')
+if [[ "$wallpaper_path_two" != *.png ]]; then
+    wallpaper_path_two=/usr/share/backgrounds/9.png
+else
+    wallpaper_path_two=$HOME/.current_wallpaper_HDMI-A-1
+fi
+
+# Define the configuration file path
+config_file="$HOME/.config/hypr/hyprlock.conf"
+
+# Use sed to replace the path value in the configuration file
+sed -i "/monitor = eDP-1/,/path =/ s|path = .*|path = $wallpaper_path_one|" "$config_file"
+sed -i "/monitor = HDMI-A-1/,/path =/ s|path = .*|path = $wallpaper_path_two|" "$config_file"
+
+hyprlock
---
+++ archcraft/.config/hypr/scripts/wallchange.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+sleep 0.5
+
+HOME=/home/santhiago
+
+# Fetch the wallpaper path and remove quotes
+wallpaper_path_one=$(wpaperctl get-wallpaper eDP-1 | tr -d '"')
+
+wallpaper_path_two=$(wpaperctl get-wallpaper HDMI-A-1 | tr -d '"')
+
+# Define the configuration file path
+config_file_one="$HOME/.current_wallpaper_eDP-1"
+
+config_file_two="$HOME/.current_wallpaper_HDMI-A-1"
+
+config_file_order="$HOME/.current_wallpaper_order"
+
+# Use sed to replace the path value in the configuration file
+cp $wallpaper_path_one $config_file_one
+
+cp $wallpaper_path_two $config_file_two
+
+if [[ "$wallpaper_path_two" == "" ]]; then
+    cp $wallpaper_path_one $config_file_order
+
+    wal -i "$wallpaper_path_one"
+else
+    cp $wallpaper_path_two $config_file_order
+
+    wal -i "$wallpaper_path_two"
+fi
+
+swaync-client -rs
