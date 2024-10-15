e1163fccfaf15384c6bc7a0e9d3dd0934a6b37fb
Santhiago Monteiro <santhiago.monteiro@hotmail.com>
2024-10-15 08:48:02 -0300

    Add exact dependencies

--- archcraft/.config/hypr/scripts/tmux/montcapital.sh
+++ archcraft/.config/hypr/scripts/tmux/montcapital.sh
@@ -6,14 +6,9 @@ tmux has-session -t $session_name
 if [ $? != 0  ]
 then
     tmux new-session -ds $session_name -x- -y-
-    tmux split-window -v -t $session_name:0.0
-    tmux resize-pane -D 15
     tmux split-window -h -t $session_name:0.0
-    tmux split-window -h -t $session_name:0.2
     tmux send-keys -t $session_name:0.0 'cd ~/Projects/php/mont-api/' C-m C-l
-    tmux send-keys -t $session_name:0.2 'cd ~/Projects/php/mont-api/' C-m C-l
-    tmux send-keys -t $session_name:0.1 'cd ~/Projects/react/mont-planejador/' C-m C-l
-    tmux send-keys -t $session_name:0.3 'cd ~/Projects/react/mont-planejador/' C-m C-l
+    tmux send-keys -t $session_name:0.1 'cd ~/Projects/php/mont-api/' C-m C-l
     tmux new-window -n outros
     tmux split-window -h -t $session_name:outros.0
     tmux select-window -t $session_name:0
